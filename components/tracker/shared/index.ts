38480aa880f396d37ed473e35efd7a138c51c239
bresha <hrvoje.bresic@gmail.com>
2025-08-31 14:07:50 +0200

    renamed header component

--- components/tracker/shared/index.ts
+++ components/tracker/shared/index.ts
@@ -1,5 +1,5 @@
 export { TrackerHeader } from './TrackerHeader';
-export { WellnessHeader } from './WellnessHeader';
+export { WellnessHeader } from '../../ui/CustomHeader';
 export { CircularProgress } from './CircularProgress';
 export { ProgressBar } from './ProgressBar';
 export { TrackerButton } from './TrackerButton';
