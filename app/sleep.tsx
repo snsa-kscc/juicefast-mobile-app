598bbefd798014c2fe49f6347393fe8ab36b6040
bresha <hrvoje.bresic@gmail.com>
2025-09-13 16:33:26 +0200

    added backend func for sleep tracker

--- app/sleep.tsx
+++ app/sleep.tsx
@@ -3,15 +3,6 @@ import { SleepTracker } from "@/components/tracker/SleepTracker";
 import { TrackerScreenWrapper } from "@/components/TrackerScreenWrapper";
 import { useRouter } from "expo-router";
 
-const INITIAL_SLEEP_DATA = {
-  sleep: {
-    hoursSlept: 7.5,
-    quality: 4,
-    startTime: new Date('2024-01-01T22:30:00'),
-    endTime: new Date('2024-01-02T06:00:00')
-  }
-};
-
 export default function Sleep() {
   const router = useRouter();
 
@@ -22,11 +13,7 @@ export default function Sleep() {
   return (
     <TrackerScreenWrapper>
       <AnimatedScreen>
-        <SleepTracker 
-          userId="user123"
-          initialSleepData={INITIAL_SLEEP_DATA}
-          onBack={handleBack}
-        />
+        <SleepTracker onBack={handleBack} />
       </AnimatedScreen>
     </TrackerScreenWrapper>
   );
