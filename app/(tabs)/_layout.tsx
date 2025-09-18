bd57d2b90f0f1dd4a7c0dc770be3b001348ecbb0
snsa-kscc <sinisa@dvasadva.com>
2025-09-19 00:28:11 +0200

    refactor: migrate tab navigation to use NativeTabs from expo-router

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,66 +1,74 @@
-import { Tabs } from "expo-router";
-import { Heart, Home, MessageCircle, Store, Users } from "lucide-react-native";
-import { Platform } from "react-native";
+import { Badge, Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
   const insets = useSafeAreaInsets();
 
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: "#ffffff",
-          borderTopWidth: 1,
-          borderTopColor: "#e5e5e5",
-          height: Platform.OS === "ios" ? 80 + insets.bottom : 70,
-          paddingBottom: Platform.OS === "ios" ? insets.bottom + 10 : 10,
-          paddingTop: 10,
-        },
-        tabBarActiveTintColor: "#000000",
-        tabBarInactiveTintColor: "#9ca3af",
-        tabBarLabelStyle: {
-          fontSize: 12,
-          fontFamily: "Lufga-Medium",
-        },
-      }}
+    <NativeTabs
+    // screenOptions={{
+    //   headerShown: false,
+    //   tabBarStyle: {
+    //     backgroundColor: "#ffffff",
+    //     borderTopWidth: 1,
+    //     borderTopColor: "#e5e5e5",
+    //     height: Platform.OS === "ios" ? 80 + insets.bottom : 70,
+    //     paddingBottom: Platform.OS === "ios" ? insets.bottom + 10 : 10,
+    //     paddingTop: 10,
+    //   },
+    //   tabBarActiveTintColor: "#000000",
+    //   tabBarInactiveTintColor: "#9ca3af",
+    //   tabBarLabelStyle: {
+    //     fontSize: 12,
+    //     fontFamily: "Lufga-Medium",
+    //   },
+    // }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf={"house.fill"} drawable="ic_menu_mylocation" />
+      </NativeTabs.Trigger>
+      {/* options={{
           title: "Home",
           tabBarIcon: ({ color, size }) => <Home size={size} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="tracker"
-        options={{
+        }} */}
+
+      <NativeTabs.Trigger name="tracker">
+        <Label>Tracker</Label>
+        <Icon sf={"heart.fill"} drawable="ic_menu_mylocation" />
+      </NativeTabs.Trigger>
+      {/* options={{
           title: "Tracker",
           tabBarIcon: ({ color, size }) => <Heart size={size} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="store"
-        options={{
+         }} */}
+
+      <NativeTabs.Trigger name="store">
+        <Label>Store</Label>
+        <Icon sf={"house.fill"} drawable="ic_menu_mylocation" />
+      </NativeTabs.Trigger>
+      {/* options={{
           title: "Store",
           tabBarIcon: ({ color, size }) => <Store size={size} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="chat"
-        options={{
+         }} */}
+
+      <NativeTabs.Trigger name="chat">
+        <Label>Chat</Label>
+        <Icon sf={"message.fill"} drawable="ic_menu_mylocation" />
+        <Badge>4</Badge>
+      </NativeTabs.Trigger>
+      {/* options={{
           title: "Chat",
           tabBarIcon: ({ color, size }) => <MessageCircle size={size} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="club"
-        options={{
+         }} */}
+
+      <NativeTabs.Trigger name="club">
+        <Label>JF Club</Label>
+        <Icon sf={"house.fill"} drawable="ic_menu_mylocation" />
+      </NativeTabs.Trigger>
+      {/* options={{
           title: "JF Club",
           tabBarIcon: ({ color, size }) => <Users size={size} color={color} />,
-        }}
-      />
-    </Tabs>
+        }} */}
+    </NativeTabs>
   );
 }
