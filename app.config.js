ea50acf28a0688351a15c9e78b980b86851d397f
snsa-kscc <sinisa@dvasadva.com>
2026-01-02 09:32:48 +0100

    chore: bump version to 1.1.1

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default () => {
     expo: {
       name: isDev ? "Juicefast DEV" : "Juicefast AI",
       slug: "juicefast-nutrition-app",
-      version: "1.1.0",
+      version: "1.1.1",
       orientation: "portrait",
       icon: "./assets/images/jf-picto-ios.png",
       scheme: isDev ? "juicefast-dev" : "juicefast-ai",
@@ -91,7 +91,7 @@ export default () => {
         },
       },
       owner: "juicefast",
-      runtimeVersion: "1.1.0",
+      runtimeVersion: "1.1.1",
       updates: {
         url: "https://u.expo.dev/6e9c5757-7446-4974-80fa-fadd2ad8ebc4",
       },
